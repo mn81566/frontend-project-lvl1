96cfb53a48e09caf2307278e36c9dd658f0fce82
mn81566 <mikhail.taskaev@gmail.com>
2021-01-13 17:33:26 +0300

    tmp

--- src/games/calculatorGame.js
+++ src/games/calculatorGame.js
@@ -1,36 +1,20 @@
-// import index from './index.js';
-// import { getRandom } from './index.js';
-// import readlineSync from 'readline-sync';
 import { init } from '../index.js';
 import { getRandom, calcWithStringOperator } from '../utils.js';
 
-// const getQuestionResponse = () => {
-//   const res = readlineSync.question('Your answer: ');
-//   return Number(res);
-// };
 const isRight = (response, writeAnswer) => Number(response) === writeAnswer;
 
 export const calc = () => {
   const gamesRuleText = 'What is the result of the expression?';
-  // let operand1 = 0;
-  // let operand2 = 0;
-  // let operator = '';
-  const operatorArr = ['+', '-', '*'];
   const operand1 = getRandom(100);
   const operand2 = getRandom(100);
+  const operatorArr = ['+', '-', '*'];
   const operator = operatorArr[getRandom(3)];
   const questionText = `Question: ${operand1} ${operator} ${operand2}`;
 
-  // eslint-disable-next-line no-eval
-  // const getWriteAnswer = () => eval(`${operand1} ${operator} ${operand2}`);
   const getWriteAnswer = () => calcWithStringOperator(operand1, operator, operand2);
   const writeAnswer = getWriteAnswer();
-  // const isRightRes = isRight(getQuestionResponse, writeAnswerFunc);
 
-  // init(gamesRuleText, questionText, isRight, writeAnswer);
   return [gamesRuleText, questionText, isRight, writeAnswer];
 };
 
-// export default calc;
-// export default init('asd', 'asdsa', true, 5);
 export default init(calc);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import readlineSync from 'readline-sync';
-// import askName from './cli.js';
 
 export const getRandom = (max) => Math.floor(Math.random() * Math.floor(max));
-// const isEven = (value) => value % 2 === 0;
 const askName = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
@@ -14,14 +12,9 @@ const askName = () => {
 const roundCount = 3;
 const getQuestionResponse = () => {
   const res = readlineSync.question('Your answer: ');
-  // if (res === 'yes') return 'yes';
-  // if (res === 'no') return 'no';
-  // return +res;
   return res;
 };
-// const isRight = (response, writeAnswer) => response === writeAnswer;
 
-// export const init = (gamesRuleText, questionText, isRightRes, writeAnswer) => {
 export const init = (params) => {
   const [gamesRuleText] = params();
   const userName = askName();
@@ -32,7 +25,6 @@ export const init = (params) => {
     const [, questionText, isRightRes, writeAnswer] = params();
     console.log(questionText);
     const response = getQuestionResponse();
-    // const writeAnswer = getWriteAnswer();
 
     if (!isRightRes(response, writeAnswer)) {
       console.log(`'${response}' is wrong answer ;(. Correct answer was '${writeAnswer}'.`);
@@ -42,5 +34,3 @@ export const init = (params) => {
   }
   console.log(`Congratulations, ${userName}`);
 };
-
-// export default init;
